53843a03d7e54984ab77d0ffc65c694b90387b60
skrispy <skrispyb@gmail.com>
2022-01-20 15:51:02 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,4 +4,6 @@ module.exports = {
     path: 'https://example.com/myaccount/',
   },
   reactStrictMode: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
