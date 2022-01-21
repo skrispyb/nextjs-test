69c6c1730168b9f54b29dc715c8ef8ebded85e15
skrispy <skrispyb@gmail.com>
2022-01-20 16:11:32 -0800

    adds fix for image paths

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { prefix } from '../prefix'
- 
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">my Next.js test project!</a>
         </h1>
 
         <p className={styles.description}>
