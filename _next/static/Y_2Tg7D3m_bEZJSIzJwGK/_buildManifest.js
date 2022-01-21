f033fa25c9f4655b7f2cec840753939446406814
skrispyb <skrispyb@gmail.com>
2022-01-21 00:12:21 +0000

    Deploying to gh-pages from @ skrispyb/nextjs-test@69c6c1730168b9f54b29dc715c8ef8ebded85e15 🚀

---
+++ _next/static/Y_2Tg7D3m_bEZJSIzJwGK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-0cf92d50943c96ed.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
